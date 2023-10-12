f787d891b77c2de8eb1897e24b7d39621a327e68
Muta-Jonathan <mutajonathan4@gmail.com>
2023-10-12 11:43:39 +0300

    testing lecle_yoyo_player

--- lib/pages/watch_page.dart
+++ lib/pages/watch_page.dart
@@ -1,12 +1,72 @@
-import 'package:flutter/cupertino.dart';
+import 'package:avvento_radio/componets/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:lecle_yoyo_player/lecle_yoyo_player.dart';
 
-class WatchPage extends StatelessWidget {
+class WatchPage extends StatefulWidget {
   const WatchPage({super.key});
 
   @override
+  State<WatchPage> createState() => _WatchPageState();
+}
+
+class _WatchPageState extends State<WatchPage> {
+  bool fullscreen = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Watch_Page"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+         const Padding(
+           padding: EdgeInsets.only(top: 40.0),
+           child: YoYoPlayer(
+                  aspectRatio: 16 / 9,
+                  url: "https://3abn-live.akamaized.net/hls/live/2010544/International/master.m3u8",
+                videoStyle: VideoStyle(
+                  showLiveDirectButton: true,
+                ),
+                  videoLoadingStyle: VideoLoadingStyle(),
+                  allowCacheFile: true,
+
+                ),
+         ),
+
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Text(
+                  'Video Title',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.share),
+                      onPressed: () {
+                        // Implement share functionality here
+                      },
+                    ),
+                    const Column(
+                      children: [
+                        Icon(Icons.favorite),
+                      ],
+                    ),
+                  ],
+                ),
+                const Text(
+                  'Video Description goes here. You can make it as long as you need.',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,6 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite
-import url_launcher_macos
 import video_player_avfoundation
 import wakelock_macos
 
@@ -30,7 +29,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
