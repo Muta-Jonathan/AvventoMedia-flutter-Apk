00bab195aa05a3246438a0e32a3208eddaf22724
Muta-Jonathan <mutajonathan4@gmail.com>
2023-10-17 09:40:45 +0300

    📻 Added notification using Just Audio background

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,20 @@ import 'package:avvento_radio/widgets/providers/spreaker_data_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:provider/provider.dart';
 
 import 'bindings/initial_binding.dart';
 import 'controller/episode_controller.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
   runApp(
     MultiProvider(
       providers: [
